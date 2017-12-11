2222c281276df2cc394a939e86ed147621c20964
Bill Miller <will.miller@utah.edu>
2017-12-10 21:36:21 -0700

    add react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import { Grid } from 'react-bootstrap'
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Free Tilt</h1>
-        </header>
-      </div>
-    );
-  }
+    render() {
+      return (
+        <Grid> 
+          <header className="App-header">
+            <h1 className="App-title">Free Tilt</h1>
+          </header>
+        </Grid >
+      );
+    }
 }
 
 export default App;
