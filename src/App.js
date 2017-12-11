100516ea99d8b46f901c7a83ff554d76bc3e9dcf
Bill Miller <will.miller@utah.edu>
2017-12-10 22:07:12 -0700

    add custom css

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import './App.css'
 import { Grid } from 'react-bootstrap'
 
 class App extends Component {
     render() {
-      return (
-        <Grid> 
-          <header className="App-header">
-            <h1 className="App-title">Free Tilt</h1>
-          </header>
-        </Grid >
-      );
+        return (
+            <Grid> 
+                <h1>Free Tilt</h1>
+            </Grid >
+        );
     }
 }
 
