f9e002e7455a5a034d14ebce61962511b5e05777
Bill Miller <will.miller@utah.edu>
2017-12-24 17:53:30 -0700

    four color deck

--- src/equitycalculator/components/buttons/card.jsx
+++ src/equitycalculator/components/buttons/card.jsx
@@ -1,9 +1,15 @@
 import * as React from 'react'
 import { Button } from 'react-bootstrap'
+import { getSuitFromHTML } from '../../'
 
-export const Card = (props) => (
-    <Button>
-        {props.rank + props.suit}
-    </Button>
-)
+export class Card extends React.Component {
+    render() {
+        let suit = getSuitFromHTML(this.props.suit.charCodeAt())
 
+        return (
+            <Button className={"cardButton " + suit}>
+                {this.props.rank + this.props.suit}
+            </Button>
+        )
+    }
+}
--- src/equitycalculator/constants/suits.js
+++ src/equitycalculator/constants/suits.js
@@ -1,3 +1,4 @@
+// unicode suits
 export const SPADES = '\u2660'
 export const HEARTS = '\u2665'
 export const DIAMONDS = '\u2666'
@@ -10,3 +11,23 @@ export const suits = [
     CLUBS
 ]
 
+// HTML entities
+const HTML_SPADES = 9824
+const HTML_HEARTS = 9829
+const HTML_DIAMONDS = 9830
+const HTML_CLUBS = 9827
+
+export const getSuitFromHTML = (entity) => {
+    switch (entity) {
+        case HTML_SPADES:
+            return 'spades'
+        case HTML_HEARTS:
+            return 'hearts'
+        case HTML_DIAMONDS:
+            return 'diamonds'
+        case HTML_CLUBS:
+            return 'clubs'
+        default:
+            return ''
+    }
+}
