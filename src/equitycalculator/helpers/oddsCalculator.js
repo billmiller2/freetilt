2171991bea9e1257fb5352d01594c508827e2a73
Bill Miller <will.miller@utah.edu>
2018-01-11 19:14:06 -0700

    sort cards

--- src/equitycalculator/helpers/oddsCalculator.js
+++ src/equitycalculator/helpers/oddsCalculator.js
@@ -1,12 +1,22 @@
 //import * as handRanks from '../constants/handRanks'
+import { ranks } from '../'
 import { generateBoard, getCardStringFromObj } from './'
 
 export const getHandRank = (handObj, board = generateBoard()) => {
-    let hand = []
+    //let hand = []
 
-    Object.entries(handObj).map((card) => {
-        hand.push(getCardStringFromObj(card[1]))
+    //Object.entries(handObj).map((card) => {
+        //hand.push(getCardStringFromObj(card[1]))
+    //})
+
+    //let cards = board.concat(hand)
+
+    //return evaluateHandStrength(cards) // future use, for now use board
+    return evaluateHandStrength(board)
+}
+
+const evaluateHandStrength = (cards) => {
+    cards.sort((a, b) => {
+        return ranks.indexOf(a[0]) - ranks.indexOf(b[0])
     })
-
-    let cards = board.concat(hand)
 }
