1d485f1ba9ca4d554aba2a13c9d39d81d0719245
Bill Miller <will.miller@utah.edu>
2018-01-12 20:00:22 -0700

    check for two pair

--- src/equitycalculator/helpers/oddsCalculator.js
+++ src/equitycalculator/helpers/oddsCalculator.js
@@ -1,7 +1,24 @@
-import * as handRankings from '../constants/handRanks'
+import * as handRanks from '../constants/handRanks'
 import { ranks } from '../'
 import { generateBoard, getCardStringFromObj } from './'
 
+/**
+ * Get hand rank
+ *
+ * Takes a hand and combines it with a board to determine best
+ * possible hand
+ *
+ * @param {object} handObj
+ * The hand object as it appears in the redux store
+ * Keep in mind that the suit strings are unicode strings
+ * Structure {
+ *     1: { rank: 'T', suit: 'S' } // card 1
+ *     2: { rank: '2', suit: 'H' } // card 2
+ * }
+ *
+ * @param {array} board  array of up to five card strings
+ * @return {string} hand rank
+ */
 export const getHandRank = (handObj, board = generateBoard()) => {
     //let hand = []
 
@@ -12,16 +29,21 @@ export const getHandRank = (handObj, board = generateBoard()) => {
     //let cards = board.concat(hand)
 
     //return evaluateHandStrength(cards) // future use, for now use board
-    board = ['TH', '9H', '6S', '2S', '2H', 'QH', 'KH']
+    board = ['TH', '5H', '6S', '2S', 'AH', '7H', 'KH']
     return evaluateHandStrength(board)
 }
 
+/**
+ * Evaluate hand strength
+ *
+ * @param {array} cards ['JD', '8C', '2H', ...]
+ */
 const evaluateHandStrength = (cards) => {
     cards.sort((a, b) => {
         return ranks.indexOf(a[0]) - ranks.indexOf(b[0])
     })
 
-    let handRanks = getRanks(cards)
+    let cardRanks = getRanks(cards)
 
     if (isStraightFlush(cards)) {
         //todo
@@ -32,8 +54,15 @@ const evaluateHandStrength = (cards) => {
         //todo
     }
 
-    if (isPair(handRanks)) {
-        return handRankings.PAIR
+    const pairCount = checkPairs(cardRanks)
+
+    if (pairCount > 0) {
+        switch (pairCount) {
+            case 2:
+                return handRanks.TWO_PAIR
+            case 1:
+                return handRanks.PAIR
+        }
     }
 }
 
@@ -46,15 +75,54 @@ const isQuads = (cards) => {
     //console.log(cards)
 }
 
-const isPair = (ranks) => {
+/**
+ * Check for pairs
+ *
+ * @param {array} ranks ['T', '7', '2', ...]
+ * @return {integer} pair count
+ */
+const checkPairs = (ranks) => {
+    let pairCount = 0
+
     for (let i = 0; i < ranks.length - 1; i++) {
-        if (ranks.indexOf(ranks[i], i + 1) >= 0) {
-            return true
+        let index = getPairIndex(ranks.slice(i))
+
+        if (index >= 0) {
+            pairCount++
+
+            if (pairCount === 2) {
+                return pairCount
+            }
+
+            ranks.splice(index, 2)
         }
     }
-    return false
+
+    return pairCount
 }
 
+/**
+ * Get pair index
+ *
+ * @param array ranks ['K', 'T', '7', ...]
+ * @return {integer} index of first pair card
+ */
+const getPairIndex = (ranks) => {
+    for (let i = 0; i < ranks.length - 1; i++) {
+        if (ranks.indexOf(ranks[i], i + 1) > 0) {
+            return i
+        }
+    }
+    return -1
+}
+
+/**
+ * Get array of ranks from array of cards
+ * Useful for calculating suit ignorant hands
+ *
+ * @param {array} cards ['AH', 'KS', '2D', ...]
+ * @return {array} ranks ['A', 'K', '2', ...]
+ */
 const getRanks = (cards) => {
     let ranks = []
     for (let i = 0; i < cards.length; i++) {
