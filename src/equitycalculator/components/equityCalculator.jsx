a70b4fa42cc6805132e871e0b6f0b764665c36f7
Bill Miller <will.miller@utah.edu>
2018-02-17 12:42:12 -0700

    improve mobile experience #1

--- src/equitycalculator/components/equityCalculator.jsx
+++ src/equitycalculator/components/equityCalculator.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { Grid, Col } from 'react-bootstrap'
+import { Grid, Col, Row } from 'react-bootstrap'
 import {
     CardsContainer,
     EquityContainer,
@@ -11,19 +11,20 @@ export class EquityCalculator extends React.Component {
         return (
             <Grid>
                 <div className="buffer-row" />
-                <Col xs={3} md={2}>
-                    <CardsContainer />
-                </Col>
-                <Col xs={1} md={1}>
-                </Col>
-                <Col xs={3} md={2}>
-                    <HandContainer number={1} />
-                    <HandContainer number={2} />
-                </Col>
-                <Col xs={3} md={2}>
+                <Col xs={12} md={2} className="col-md-push-5">
                     <EquityContainer />
                 </Col>
-                <Col xs={2} md={5}>
+                <Col xs={12} md={2} className="col-md-push-1">
+                    <Col xs={6} md={12}>
+                        <HandContainer number={1} />
+                    </Col>
+                    <Col xs={6} md={12}>
+                        <HandContainer number={2} />
+                    </Col>
+                </Col>
+                <Row className="visible-xs-block buffer-row" />
+                <Col xs={12} md={2} className="col-md-pull-4">
+                    <CardsContainer />
                 </Col>
             </Grid>
         )
