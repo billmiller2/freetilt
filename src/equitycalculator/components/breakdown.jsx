03f5e15400a56a4cb9f4bc7830e6bb6fa0808a57
Bill Miller <will.miller@utah.edu>
2018-05-13 13:50:29 -0600

    breakdown table #4

--- src/equitycalculator/components/breakdown.jsx
+++ src/equitycalculator/components/breakdown.jsx
@@ -1,4 +1,35 @@
 import * as React from 'react'
 
-export const Breakdown = () => <h4>Breakdown</h4>
+export const Breakdown = (props) => {
+    const { equities, hands } = props
 
+    return (
+        <div>
+            <h4>Breakdown</h4>
+            <table className="table">
+                <tbody>
+                    <tr>
+                        <th></th>
+                        <td>
+                            {hands[1][1].rank
+                                + hands[1][1].suit
+                                + hands[1][2].rank
+                                + hands[1][2].suit}
+                        </td>
+                        <td>
+                            {hands[2][1].rank
+                                + hands[2][1].suit
+                                + hands[2][2].rank
+                                + hands[2][2].suit}
+                        </td>
+                    </tr>
+                    <tr>
+                        <th>Equity</th>
+                        <td>{equities[0]}</td>
+                        <td>{equities[1]}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    )
+}
--- src/equitycalculator/containers/breakdownContainer.jsx
+++ src/equitycalculator/containers/breakdownContainer.jsx
@@ -1,8 +1,14 @@
 import { connect } from 'react-redux'
 import { Breakdown } from '../'
 
-const mapStateToProps = () => {
-    return {}
+const mapStateToProps = (state) => {
+    const { savedEquities } = state.equityReducer
+    const { equities, hands } = savedEquities[savedEquities.length - 1]
+
+    return {
+        equities,
+        hands
+    }
 }
 
 export const BreakdownContainer = connect(
