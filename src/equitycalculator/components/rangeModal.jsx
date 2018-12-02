6e7eb819cb0df39a1165db64078e08d88838200b
Bill Miller <will.miller@utah.edu>
2018-12-02 14:41:30 -0700

    range matrix #5

--- src/equitycalculator/components/rangeModal.jsx
+++ src/equitycalculator/components/rangeModal.jsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Modal } from 'react-bootstrap'
 
-export const RangeModal = (props) => {
-    return (
-        <Modal show={props.show} onHide={props.onClose}>
-            <Modal.Header closeButton>
-            </Modal.Header>
-            <Modal.Body>
-                <h4>Range</h4>
-            </Modal.Body>
-        </Modal>
-    )
+import { ranks } from '../'
+
+export class RangeModal extends Component {
+    render() {
+        const { show, onClose } = this.props
+
+        return (
+            <Modal show={show} onHide={onClose}>
+                <Modal.Header closeButton>
+                </Modal.Header>
+                <Modal.Body>
+                    <h4>Range</h4>
+                    {ranks.map((rankOne, i) => {
+                        let suited = 'o'
+                        return (
+                            <div className="row rangeRow" key={rankOne + i}>
+                                {ranks.map(rankTwo => {
+                                    if (suited.length === 0) {
+                                        suited = 's'
+                                    }
+
+                                    if (rankOne === rankTwo) {
+                                        suited = ''
+                                    }
+
+                                    let btnClass = ''
+
+                                    switch (suited) {
+                                        case 's':
+                                            btnClass = 'suitedRangeBtn'
+                                            break;
+                                        case 'o':
+                                            btnClass = 'offsuitRangeBtn'
+                                            break;
+                                        default:
+                                            btnClass = 'pairRangeBtn'
+                                    }
+
+                                    return (
+                                        <button className={"btn btn-sm rangeBtn " + btnClass} key={rankOne + rankTwo}>
+                                            {rankOne + rankTwo + suited}
+                                        </button>
+                                    )
+                                })}
+                            </div>
+                        )
+                    })}
+                </Modal.Body>
+            </Modal>
+        )
+    }
 }
